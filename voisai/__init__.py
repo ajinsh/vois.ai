9c9e5da46d54aad0b48c2a92be67b0acc84431ad
Ajinkya Shinde <ajinkya.shinde384@gmail.com>
2018-07-02 18:47:24 -0700

    Revised files

--- voisai/__init__.py
+++ voisai/__init__.py
@@ -4,7 +4,26 @@ from flask import Flask,render_template, request,json
 app = Flask("__name__")
 
 
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/')
+def display():
+    return render_template('home.html')
+
+@app.route('/recording/')
+def record():
+    return render_template('recording.html')
+
+@app.route('/analyze/', methods=['POST'])
+def analyze():
+	if request.method == 'POST':
+		result = request.form['sp_area']
+	return json.dumps({'status':'OK','result':result});
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
-
+    
